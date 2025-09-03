41829d30a43bfbdaf651ba2e6c6d0409fde0c9e1
yhon <ydenvalencia@gmail.com>
2025-09-03 10:33:24 -0500

    Fix error origin Cors

--- Libreria.Backend/src/main.py
+++ Libreria.Backend/src/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Librería API")
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://libreriaapp.netlify.app/",
+    "https://libreriaapp.netlify.app",
 ]
 
 app.add_middleware(
